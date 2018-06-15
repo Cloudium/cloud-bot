1473f835a88c4107e48348686a554cbbc6e2221b
Cloudium <cloudium@gmx.us>
2018-06-15 10:23:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 let prefix = "c!";
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setGame('c!help !')
 });
 
 client.on('message', message => {
+    
+    
     if (message.content.startsWith = prefix){
+        
+        
          if (message.content === prefix + 'ping') {
     	    message.reply('pong');
-       	}
+       	};
+        
+        if(message.content === prefix + "info"){
+            message.reply("Hello ! I am the official discord bot of the Clouds server nice to meet you !")
+        };
+        
+      
+        if(message.content === prefix + ""){};
+        
+        
     }});
 
 // THIS  MUST  BE  THIS  WAY
