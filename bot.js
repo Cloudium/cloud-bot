153d2c4f1935b897b16e6af9ad1c999f34f73140
Cloudium <cloudium@gmx.us>
2018-06-17 11:28:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,25 +26,24 @@ client.on('message', message => {
         if(message.content.startsWith(prefix + "play")){
 		console.log("recieved");
 		message.channel.send("processing request");
-		exports.run = async (client, message, args) =>{
 			if(!message.member.voiceChannel){return message.channel.Send("please connect to a voice channel")};
 		
 			if(message.guild.me.voiceChannel){return message.channel.send("Sorry the bot is already connected to the guild.")};
 		
 			if(!args[0]){return message.channel.send("Sorry, please input a url following the command")};
 		
-			let validate = await ytdl.validateURL(args[0]);
+			let validate = ytdl.validateURL(args[0]);
 		
 			if(!validate){return message.channel.send("sorry please put a **valid** url following the comman")};
 			
-			let info = await ydtl.getInfo(args[0]);
+			let info = ydtl.getInfo(args[0]);
 		
-			let connection = await message.member.voiceChannel.join();
+			let connection = message.member.voiceChannel.join();
 		
-			let dispatcher = await connection.play(ytdl(args[0], {filter: "audioonly"}));
+			let dispatcher = connection.play(ytdl(args[0], {filter: "audioonly"}));
 		
 		message.channel.send("Now playing: ${info.title}");
-	}};
+	};
         
         
     }});
