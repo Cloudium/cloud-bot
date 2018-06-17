7e304fdff981767d3c4cb7912fdbf910df804918
Cloudium <cloudium@gmx.us>
2018-06-17 11:26:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,28 +19,29 @@ client.on('message', message => {
        	};
         
         if(message.content === prefix + "info"){
-            message.reply("Hello ! I am the official discord bot of the Clouds server nice to meet you !")
+            message.reply("Hello ! I am the official discord bot of the Clouds server nice to meet you !");
         };
         
       
         if(message.content.startsWith(prefix + "play")){
-	console.log("recieved");
-	exports.run = async (client, message, args) =>{
-		if(!message.member.voiceChannel){return message.channel.Send("please connect to a voice channel")};
+		console.log("recieved");
+		message.channel.send("processing request");
+		exports.run = async (client, message, args) =>{
+			if(!message.member.voiceChannel){return message.channel.Send("please connect to a voice channel")};
 		
-		if(message.guild.me.voiceChannel){return message.channel.send("Sorry the bot is already connected to the guild.")};
+			if(message.guild.me.voiceChannel){return message.channel.send("Sorry the bot is already connected to the guild.")};
 		
-		if(!args[0]){return message.channel.send("Sorry, please input a url following the command")};
+			if(!args[0]){return message.channel.send("Sorry, please input a url following the command")};
 		
-		let validate = await ytdl.validateURL(args[0]);
+			let validate = await ytdl.validateURL(args[0]);
 		
-		if(!validate){return message.channel.send("sorry please put a **valid** url following the comman")};
+			if(!validate){return message.channel.send("sorry please put a **valid** url following the comman")};
+			
+			let info = await ydtl.getInfo(args[0]);
 		
-		let info = await ydtl.getInfo(args[0]);
+			let connection = await message.member.voiceChannel.join();
 		
-		let connection = await message.member.voiceChannel.join();
-		
-		let dispatcher = await connection.play(ytdl(args[0], {filter: "audioonly"}));
+			let dispatcher = await connection.play(ytdl(args[0], {filter: "audioonly"}));
 		
 		message.channel.send("Now playing: ${info.title}");
 	}};
