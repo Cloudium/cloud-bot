63703387679616de11aa0277596264e891ba8bab
Cloudium <cloudium@gmx.us>
2018-06-17 12:48:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on('message', message => {
 		message.channel.send('Now playing: ${info.title}');
 	};
 	
-	if(message.content === prefix + "disconnect"){bot.voiceConnections.get(guildid).disconnect());}
+	if(message.content === prefix + "disconnect"){bot.voiceConnections.get(guildid).disconnect();}
 	    
         
         
